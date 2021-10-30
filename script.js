3be5f6dcba0ae42d74230b6d9ea8c17c66874b1f
anacarolinesilva <anasilva.caroline@hotmail.com>
2021-10-29 21:54:08 -0300

    commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var contador = 1
+do {
+    console.log(`Passo ${contador}`)
+    contador++ // contador = contador + 1
+} while (contador <= 6)
+
+/*
+var contador = 1
+while (contador <= 6) {
+    console.log(`Passo ${contador}`)
+    contador++ // contador = contador + 1
+}
+*/
+
+/*
+console.log('Passo 1')
+console.log('Passo 2')
+console.log('Passo 3')
+console.log('Passo 4')
+console.log('Passo 5')
+console.log('Passo 6')
+*/
+
